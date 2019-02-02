4b8f6d23d508c66b9267b78aa03638cbee998666
denis-skripnik <scadens@yandex.ru>
2019-02-02 23:52:13 +0700

    home.php and...
    add viz to home.php

--- home.php
+++ home.php
@@ -1,10 +1,10 @@
 <?php
 // Сервисы, главная:
 if (!isset($array_url[0])) {
-    $title = "dpos.space | Сайт с сервисами для DPOS блокчейнов Steem, Golos и WLS";
-    $meta_keywords = "steem, golos, WLS, клиент, сайт, сервисы, проекты, dpos";
-    $meta_description = "Сайт с сервисами для DPOS блокчейнов Golos, Steem, WLS. Просмотр профиля, фид лента, бекап постов и другое.";
-    $h1 = "Сервисы для блокчейнов Steem, Golos и WLS";
+    $title = "dpos.space | Сайт с сервисами для DPOS блокчейнов Steem, Golos, Viz и Whaleshares";
+    $meta_keywords = "steem, golos, Viz, whaleshares, клиент, сайт, сервисы, проекты, dpos";
+    $meta_description = "Сайт с сервисами для DPOS блокчейнов Golos, Steem, Viz и Whaleshares. Просмотр профиля, фид лента, бекап постов и другое.";
+    $h1 = "Сервисы для блокчейнов Steem, Golos, Viz и Whaleshares";
     $description = '<p>Создатель сервисов - незрячий программист Денис Скрипник (@denis-skripnik в любом из трёх блокчейнов)</p>';
-$footer_text = "работать с блокчейнами Steem, Golos и WLS. Здесь вы найдёте несколько сервисов, функционал которых увеличиваются, да и число сервисов растёт." . $chain . " в удобном виде.";
+$footer_text = "работать с блокчейнами Steem, Golos, Viz и Whaleshares. Здесь вы найдёте несколько сервисов, функционал которых увеличиваются, да и число сервисов растёт." . $chain . " в удобном виде.";
 	} // Конец условия для главной
