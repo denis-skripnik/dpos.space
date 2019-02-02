189d673469cd2b793639c7c368a5829a74262102
denis-skripnik <scadens@yandex.ru>
2019-02-02 12:30:03 +0700

    delete js.php and add js code in index.php
    Перенёс Javascript код из php файлов в index.php calc сервиса.

--- calc/index.php
+++ calc/index.php
@@ -12,9 +12,33 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/template/footer.php';
 ?>
   <script>
   $(document).ready(function(){
-<?php require $_SERVER['DOCUMENT_ROOT'].'/calc/upvotes/js.php';
-require $_SERVER['DOCUMENT_ROOT'].'/calc/vests-gests/js.php';
-?>
+    $("#result_power").click(function(){
+var steem_power = document.getElementById('sp').value;
+var vote_power = document.getElementById('vp').value;
+<?php if ($chain != 'viz') { ?>
+    var vote_weight = document.getElementById('vote_weight').value
+	  $("#let1").load("/calc/upvotes/ajax.php","sp=" + steem_power + "&charge=" + vote_power + "&vote_weight=" + vote_weight + "&chain=<?php echo $array_url[1]; ?>&array_url[0]=<?php echo $array_url[0]; ?>&array_url[1]=<?php echo $array_url[1]; ?>", function(result, status) {
+          if (status === 'error') {
+              console.log(result);
+          }
+      });
+   });
+   <?php } else { ?>
+    $("#let1").load("/calc/upvotes/ajax.php","sp=" + steem_power + "&charge=" + vote_power + "&chain=<?php echo $array_url[1]; ?>&array_url[0]=<?php echo $array_url[0]; ?>&array_url[1]=<?php echo $array_url[1]; ?>", function(result, status) {
+          if (status === 'error') {
+              console.log(result);
+          }
+      });
+   });
+<?php } ?>
+$("#result_vests").click(function(){
+var sp_tec = document.getElementById('sp_tec').value
+	  $("#let2").load("/calc/vests-gests/ajax.php","sp-tec=" + sp_tec + "&chain=<?php echo $array_url[1]; ?>&array_url[0]=<?php echo $array_url[0]; ?>&array_url[1]=<?php echo $array_url[1]; ?>", function(result, status) {
+          if (status === 'error') {
+              console.log(result);
+          }
+      });
+   });
   });             
 </script>                                                      
 <?php
