4114f5afeed7ffaf4c3e427dc8fee9fce3160ea1
denis-skripnik <scadens@yandex.ru>
2020-08-17 17:43:19 +0700

    add links to tokens in home page, golos/top service

--- blockchains/golos/apps/top/content.php
+++ blockchains/golos/apps/top/content.php
@@ -1,6 +1,12 @@
 <?php if (!defined('NOTLOAD')) exit('No direct script access allowed');
 return '<h2>Выберите токен</h2>
-<ol><li><a href="'.$conf['siteUrl'].'golos/top/GBG">GBG</a></li>
-<li><a href="'.$conf['siteUrl'].'golos/top/GOLOS">GOLOS</a></li>
-<li><a href="'.$conf['siteUrl'].'golos/top/GP">СГ</a></li></ol>
+<ol><li><a href="'.$conf['siteUrl'].'golos/top/gbg">GBG</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/golos">GOLOS</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/tip_balance">TIP-баланс</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/gp">СГ</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/delegated_gp">Делегированная СГ</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/received_gp">Полученная делегированием СГ</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/effective_gp">Эффективная СГ (личная - делегированная + полученна делегированием)</a></li>
+<li><a href="'.$conf['siteUrl'].'golos/top/reputation">Репутация</a></li>
+</ol>
 '; ?>
