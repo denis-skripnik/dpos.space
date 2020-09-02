184a00882ef5373169e10cf157ee7a01159f428b
denis-skripnik <scadens@yandex.ru>
2020-09-02 14:09:57 +0700

    Add info about update

--- blockchains/golos/apps/witnesses-rewards/content.php
+++ blockchains/golos/apps/witnesses-rewards/content.php
@@ -2,14 +2,15 @@
   global $conf;
     $html = file_get_contents('http://138.201.91.11:3000/golos-api?service=witnesses');
     $table = json_decode($html, true);
-    $content = '<table id="table"><thead><tr><th>Логин</th><th>за вчерашний день</th><th>за сегодня</th><th>за прошлый месяц</th><th>за текущий месяц</th></tr></thead><tbody id="target">';
+    $content = '<p><strong>Обновление происходит в полночь по GMT, но не все сразу делегаты обновляются, а те, которые подписывают блоки.</strong></p>
+<table id="table"><thead><tr><th>Логин</th><th>за вчерашний день</th><th>за сегодня</th><th>за прошлый месяц</th><th>за текущий месяц</th></tr></thead><tbody id="target">';
     if ($table) {
     foreach ($table as $witness) {
       $content .= '<tr align="right"><td><a href="'.$conf['siteUrl'].'golos/profiles/'.$witness['login'].'/witness" target="_blank">'.$witness['login'].'</a></td>
-    <td>'.$witness['old_daily_profit'].'</td>
-    <td>'.$witness['now_daily_profit'].'</td>
-    <td>'.$witness['old_monthly_profit'].'</td>
-    <td>'.$witness['now_monthly_profit'].'</td>
+    <td>'.round($witness['old_daily_profit'], 3).'</td>
+    <td>'.round($witness['now_daily_profit'], 3).'</td>
+    <td>'.round($witness['old_monthly_profit'], 3).'</td>
+    <td>'.round($witness['now_monthly_profit'], 3).'</td>
     </tr>';
     }
     }
