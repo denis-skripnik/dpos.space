874c2488c42bd80f15b333ba4aeed7986d364387
denis-skripnik <scadens@yandex.ru>
2021-02-08 21:20:51 +0700

    Update viz/buy: Deleted two child pages, add menaskop.xyz in links for buy Viz

--- blockchains/viz/apps/buy/index.php
+++ blockchains/viz/apps/buy/index.php
@@ -1,15 +1,2 @@
 <?php if (!defined('NOTLOAD')) exit('No direct script access allowed');
-require_once 'functions.php';
-function generateAppPages($blockchain_snippet) {
-    global $conf;
-    $page = pageUrl()[2];
-    if (is_dir(__DIR__.'/pages/'.$page)) {
-$page_config = configs(__DIR__.'/pages/'.$page.'/config.json');
-$data = $page_config;
-$data['content'] = $blockchain_snippet;
-$data['content'] .= require_once(__DIR__.'/pages/'.$page.'/content.php');
-}
-    return $data;
-}
-    $data = generateAppPages($blockchain_snippet);
-    ?>
+?>
