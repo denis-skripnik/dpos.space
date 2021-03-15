b04e9cdafe02d71e9eb4d0d20bdafc39d5ed9644
denis-skripnik <scadens@yandex.ru>
2021-03-15 17:58:54 +0700

    Update buy VIZ list

--- blockchains/viz/apps/buy/content.php
+++ blockchains/viz/apps/buy/content.php
@@ -1,6 +1,5 @@
 <?php if (!defined('NOTLOAD')) exit('No direct script access allowed');
 return '<h2>Вы можете купить VIZ</h2>
-<ol><li>Через обменник <a href="https://coins.black" target="_blank">Coins.black</a></li>
-<li>Через <a href="https://menaskop.xyz/" target="_blank">menaskop.xyz</a></li>
+<ol><li>Через <a href="https://menaskop.xyz/" target="_blank">menaskop.xyz</a></li>
 <li>На бирже: <a href="https://control.viz.world/media/@urri77/покупка-viz-за-usdt-на-бирже-рудекс/" target="_blank">читать статью</a></li>
 </ol>'; ?>
