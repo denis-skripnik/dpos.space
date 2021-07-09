8f1fe0d42beb46294926666e06696077fb8790f5
Denis <scadens@yandex.ru>
2021-07-10 00:00:23 +0700

    Add minter pool to buy VIZ list

--- blockchains/viz/apps/buy/content.php
+++ blockchains/viz/apps/buy/content.php
@@ -1,5 +1,6 @@
 <?php if (!defined('NOTLOAD')) exit('No direct script access allowed');
 return '<h2>Вы можете купить VIZ</h2>
-<ol><li>Через <a href="https://menaskop.xyz/" target="_blank">menaskop.xyz</a></li>
+<ol><li>Через Minter. Пул <a href="https://chainik.io/pool/BIP/VIZCHAIN" target="_blank">BIP/VIZCHAIN</a>, <a href="https://viz.media/zapusk-shlyuza-viz-v-minter/" target="_blank">Инструкция</a></li>
+<li>Через <a href="https://menaskop.xyz/" target="_blank">menaskop.xyz</a></li>
 <li>На бирже: <a href="https://control.viz.world/media/@urri77/покупка-viz-за-usdt-на-бирже-рудекс/" target="_blank">читать статью</a></li>
 </ol>'; ?>
