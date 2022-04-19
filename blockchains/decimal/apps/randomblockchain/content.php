e04f068491e16773a51e070596b0800dddae0611
Denis <scadens@yandex.ru>
2022-04-19 17:55:18 +0700

    Update randomblockchain GET parameters

--- blockchains/decimal/apps/randomblockchain/content.php
+++ blockchains/decimal/apps/randomblockchain/content.php
@@ -1,6 +1,6 @@
 <?php if (!defined('NOTLOAD')) exit('No direct script access allowed');
 global $conf;
-if (isset($_GET)) {
+if (isset($_GET) && count($_GET) > 0) {
     $data['title'] .= ' | Блоки '.$_GET['block1'].' и '.$_GET['block2'].', участников '.$_GET['participants'];
 }
 if (!empty($_GET)) {
