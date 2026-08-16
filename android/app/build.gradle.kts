a49b3bc9f9f76a70d4899f0bf017beaee94043a8
Denis Skripnik <deniska299@gmail.com>
2026-08-16 06:57:14 +0000

    Use async Golos vote broadcast like web

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "space.dpos.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 58
-        versionName = "0.1.57"
+        versionCode = 59
+        versionName = "0.1.58"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "DPOS_WEB_URL", "\"https://dpos.blinddev.xyz/\"")
     }
